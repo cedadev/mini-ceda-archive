39265df79202788a41d13e58a599c4a64b5abbde
agstephens <ag.stephens@stfc.ac.uk>
2021-07-27 13:00:14 +0100

    Added new cru_ts_4.05 file

--- archive_maker/generate-cru_ts.py
+++ archive_maker/generate-cru_ts.py
@@ -1,9 +1,12 @@
 import os
 import glob
+import shutil
 import subprocess as sp
+import time
 
 
-data_dir = '/badc/cru/data/cru_ts/cru_ts_4.04/data'
+data_dir_4_04 = '/badc/cru/data/cru_ts/cru_ts_4.04/data'
+data_dir_4_05 = '/badc/cru/data/cru_ts/cru_ts_4.05/data'
 mini_archive = './archive'
 
 
@@ -28,33 +31,62 @@ def create_subset_file(fpath):
     lon_selector = '-d lon,,,100'
     extra = ''
 
-    cmd = f'ncks {extra} {lat_selector} {lon_selector} --variable {var_id} {fpath} {output_path}'
-    print(f'[INFO] Running: {cmd}')
+    if fpath.endswith(".gz"):
+        tmp_gzip = os.path.join(mini_dir, 'temp.nc.gz')
+        print(f'[INFO] Gunzipping to temporary file: {tmp_gzip}')
+        shutil.copy(fpath, tmp_gzip)
 
+        sp.call(f'gunzip {tmp_gzip}', shell=True)
+
+        nc_gz_path = output_path
+        input_path = os.path.splitext(tmp_gzip)[0]
+        output_path = os.path.splitext(output_path)[0]
+    else:
+        input_path = fpath
+
+    cmd = f'ncks {extra} {lat_selector} {lon_selector} --variable {var_id} {input_path} {output_path}'
+    print(f'[INFO] Running: {cmd}')
     sp.call(cmd, shell=True)
+
+    if fpath.endswith(".gz"):
+        print(f'[INFO] Removing: {input_path}')
+        os.remove(input_path)
+
+        print(f'[INFO] Gzipping: {output_path}')
+        sp.call(f'gzip {output_path}', shell=True)
+        output_path = nc_gz_path
 
     print(f'[INFO] OUTPUT: {output_path}')
 
 
 def process_var(var_dir):
     
-    nc_files = glob.glob(f'{var_dir}/*.nc')
-    if len(nc_files) != 1:
-        raise Exception(f'Expected one file ".nc" file in directory, but found: {nc_files}')
+    # Try: "*.nc" and then "*.nc.gz" if no "*.nc" files
+    files = glob.glob(f'{var_dir}/*.nc') or glob.glob(f'{var_dir}/*.1901.2*.nc.gz')
 
-    fpath = nc_files[0]
+    if len(files) != 1:
+        raise Exception(f'Expected one data file in directory, but found: {files}')
+
+    fpath = files[0]
     create_subset_file(fpath)
 
 
-def main(dr=data_dir):
+def main():
 
-    var_ids = os.listdir(data_dir)
+    dir_var_map = {
+        #data_dir_4_04: os.listdir(data_dir_4_04),
+        data_dir_4_05: ["wet"]
+    } 
 
-    for var_id in var_ids:
-        print(f'[INFO] Working on: {var_id}')
+    for dr in dir_var_map:
+        print(f'[INFO] Working on: {dr}')
+        var_ids = dir_var_map[dr]
 
-        var_dir = os.path.join(dr, var_id)
-        process_var(var_dir)
+        for var_id in var_ids:
+            print(f'[INFO] Working on: {var_id}')
+
+            var_dir = os.path.join(dr, var_id)
+            process_var(var_dir)
 
 
 if __name__ == '__main__':
